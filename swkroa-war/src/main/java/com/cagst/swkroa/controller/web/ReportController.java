9ebabcf759fee13a7b8b5afe67f860c2740f8aaf
Craig Gaskill <cgaskill23824@att.net>
2014-09-04 06:32:47 -0500

    Completed Membership Past Due report.

--- swkroa-war/src/main/java/com/cagst/swkroa/controller/web/ReportController.java
+++ swkroa-war/src/main/java/com/cagst/swkroa/controller/web/ReportController.java
@@ -1,6 +1,5 @@
 package com.cagst.swkroa.controller.web;
 
-import com.cagst.swkroa.codevalue.CodeValue;
 import com.cagst.swkroa.codevalue.CodeValueRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,11 +8,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
-
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 /**
  * Handles and retrieves the Report pages depending on the URI template.
@@ -34,23 +28,50 @@ public final class ReportController {
    *
    * @return The name of the page.
    */
-  @RequestMapping(value = "/membership_listing", method = RequestMethod.GET)
+  @RequestMapping(value = "/membership/listing", method = RequestMethod.GET)
   public ModelAndView getMembershipListingReport() {
     LOGGER.info("Received request to show Membership Listing report options.");
 
-    List<CodeValue> types = codeValueRepo.getCodeValuesForCodeSetByMeaning(CodeValueRepository.MEMBERSHIP_TYPE);
-    Collections.sort(types);
-
-    ModelAndView mav = new ModelAndView("report/membership_listing");
-    mav.addObject("membershipTypes", types);
+    ModelAndView mav = new ModelAndView("report/membership/listing");
 
     return mav;
   }
 
-  @RequestMapping(value = "/membership_listing", method = RequestMethod.POST)
+  /**
+   * Handles the request to run/generate the Membership Listing report page.
+   *
+   * @return The generated report.
+   */
+  @RequestMapping(value = "/membership/listing", method = RequestMethod.POST)
   public ModelAndView runMembershipListingReport() {
     LOGGER.info("Received request to run Membership Listing report.");
 
     return new ModelAndView("membershipListingReport");
+  }
+
+  /**
+   * Handles and retrieves the Membershp Past Due report page.
+   *
+   * @return The name of the page.
+   */
+  @RequestMapping(value = "/membership/pastdue", method = RequestMethod.GET)
+  public ModelAndView getMembershipPastDueReport() {
+    LOGGER.info("Received request to show Membership Past Due report options.");
+
+    ModelAndView mav = new ModelAndView("report/membership/pastdue");
+
+    return mav;
+  }
+
+  /**
+   * Handles the request to run/generate the Membership Past Due report page.
+   *
+   * @return The generated report.
+   */
+  @RequestMapping(value = "/membership/pastdue", method = RequestMethod.POST)
+  public ModelAndView runMembershipPastDueReport() {
+    LOGGER.info("Received request to run Membership Listing report.");
+
+    return new ModelAndView("membershipPastDueReport");
   }
 }
