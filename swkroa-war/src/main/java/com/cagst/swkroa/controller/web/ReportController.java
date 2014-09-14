36e9bd12364c9c3081cf6eff35579cfc0c629669
Craig Gaskill <cgaskill23824@att.net>
2014-09-14 07:50:51 -0500

    Added ability to generate PDF or XLS with properly named filename when downloaded.

--- swkroa-war/src/main/java/com/cagst/swkroa/controller/web/ReportController.java
+++ swkroa-war/src/main/java/com/cagst/swkroa/controller/web/ReportController.java
@@ -1,6 +1,6 @@
 package com.cagst.swkroa.controller.web;
 
-import com.cagst.swkroa.codevalue.CodeValueRepository;
+import com.cagst.swkroa.report.JasperReportsViewFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +8,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.jasperreports.AbstractJasperReportsView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.sql.DataSource;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * Handles and retrieves the Report pages depending on the URI template.
@@ -20,8 +27,13 @@ import org.springframework.web.servlet.ModelAndView;
 public final class ReportController {
   private static final Logger LOGGER = LoggerFactory.getLogger(ReportController.class);
 
+  private static final DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+
+  private static final String MEMBERSHIP_LISTING  = "/WEB-INF/reports/jasper/MembershipListingReport.jasper";
+  private static final String MEMBERSHIP_PAST_DUE = "/WEB-INF/reports/jasper/MembershipPastDueReport.jasper";
+
   @Autowired
-  private CodeValueRepository codeValueRepo;
+  private DataSource dataSource;
 
   /**
    * Handles and retrieves the Membership Listing report page.
@@ -32,9 +44,7 @@ public final class ReportController {
   public ModelAndView getMembershipListingReport() {
     LOGGER.info("Received request to show Membership Listing report options.");
 
-    ModelAndView mav = new ModelAndView("report/membership/listing");
-
-    return mav;
+    return new ModelAndView("report/membership/listing");
   }
 
   /**
@@ -43,13 +53,19 @@ public final class ReportController {
    * @return The generated report.
    */
   @RequestMapping(value = "/membership/listing", method = RequestMethod.POST)
-  public ModelAndView runMembershipListingReport() {
+  public ModelAndView runMembershipListingReport(final HttpServletRequest request) {
     LOGGER.info("Received request to run Membership Listing report.");
 
-    ModelAndView mav = new ModelAndView("membershipListingReport");
-    mav.addObject("format", "pdf");
+    String reportFilename = "Membership_Listing_" + dateFormat.format(new Date());
 
-    return mav;
+    AbstractJasperReportsView view = JasperReportsViewFactory.getJasperReportsView(
+        request,
+        dataSource,
+        MEMBERSHIP_LISTING,
+        JasperReportsViewFactory.REPORT_FORMAT_PDF,
+        reportFilename);
+
+    return new ModelAndView(view);
   }
 
   /**
@@ -61,9 +77,7 @@ public final class ReportController {
   public ModelAndView getMembershipPastDueReport() {
     LOGGER.info("Received request to show Membership Past Due report options.");
 
-    ModelAndView mav = new ModelAndView("report/membership/pastdue");
-
-    return mav;
+    return new ModelAndView("report/membership/pastdue");
   }
 
   /**
@@ -72,12 +86,19 @@ public final class ReportController {
    * @return The generated report.
    */
   @RequestMapping(value = "/membership/pastdue", method = RequestMethod.POST)
-  public ModelAndView runMembershipPastDueReport() {
+  public ModelAndView runMembershipPastDueReport(final HttpServletRequest request) {
     LOGGER.info("Received request to run Membership Listing report.");
 
-    ModelAndView mav = new ModelAndView("membershipPastDueReport");
-    mav.addObject("format", "xls");
-    mav.addObject("ReportTitle", "Past_Due_Memberships_2014-09-09.xlsx");
+    String reportFilename = "Membership_PastDue_" + dateFormat.format(new Date());
+
+    AbstractJasperReportsView view = JasperReportsViewFactory.getJasperReportsView(
+        request,
+        dataSource,
+        MEMBERSHIP_PAST_DUE,
+        JasperReportsViewFactory.REPORT_FORMAT_XLS,
+        reportFilename);
+
+    ModelAndView mav = new ModelAndView(view);
     mav.addObject("IS_IGNORE_PAGINATION", true);
 
     return mav;
---
+++ swkroa-war/src/main/java/com/cagst/swkroa/report/JasperReportsViewFactory.java
@@ -0,0 +1,79 @@
+package com.cagst.swkroa.report;
+
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.util.Assert;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+import org.springframework.web.servlet.view.jasperreports.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.sql.DataSource;
+import java.util.Properties;
+
+/**
+ * Factory that will return a ...
+ *
+ * @author Craig Gaskill
+ * @version 1.0.0
+ */
+public class JasperReportsViewFactory {
+  private static final Logger LOGGER = LoggerFactory.getLogger(JasperReportsViewFactory.class);
+
+  /**
+   * Constant that defines "Content-Disposition" header.
+   */
+  private static final String HEADER_CONTENT_DISPOSITION = "Content-Disposition";
+
+  public static final String REPORT_FORMAT_PDF = "pdf";
+  public static final String REPORT_FORMAT_XLS = "xls";
+  public static final String REPORT_FORMAT_CSV = "csv";
+
+  public static AbstractJasperReportsView getJasperReportsView(final HttpServletRequest request,
+                                                               final DataSource dataSource,
+                                                               final String reportUrl,
+                                                               final String reportFormat,
+                                                               final String reportFilename) {
+
+    Assert.notNull(request, "Assertion Failed - argument [request] cannot be null");
+    Assert.notNull(dataSource, "Assertion Failed - argument [dataSource] cannot be null");
+    Assert.hasText(reportUrl, "Assertion Failed - argument [reportUrl] cannot be null or empty");
+
+    String format = StringUtils.isEmpty(reportFilename) ? REPORT_FORMAT_PDF : reportFormat;
+
+    // set possible content headers
+    Properties availableHeaders = new Properties();
+    availableHeaders.put("html", "inline; filename=" + reportFilename + ".html");
+    availableHeaders.put("csv", "inline; filename=" + reportFilename + ".csv");
+    availableHeaders.put("pdf", "inline; filename=" + reportFilename + ".pdf");
+    availableHeaders.put("xls", "inline; filename=" + reportFilename + ".xls");
+
+    AbstractJasperReportsSingleFormatView view;
+    if (REPORT_FORMAT_PDF.equals(format)) {
+      view = new JasperReportsPdfView();
+    } else if (REPORT_FORMAT_XLS.equals(format)) {
+      view = new JasperReportsXlsView();
+    } else if (REPORT_FORMAT_CSV.equals(format)) {
+      view = new JasperReportsCsvView();
+    } else {
+      LOGGER.error("Unsupported report format [{}]", format);
+      return null;
+    }
+
+    WebApplicationContext appCtx = WebApplicationContextUtils.getWebApplicationContext(request.getSession().getServletContext());
+
+    view.setJdbcDataSource(dataSource);
+    view.setUrl(reportUrl);
+    view.setApplicationContext(appCtx);
+
+    if (StringUtils.isNotBlank(reportFormat)) {
+      Properties headers = new Properties();
+      headers.put(HEADER_CONTENT_DISPOSITION, availableHeaders.get(format));
+
+      view.setHeaders(headers);
+    }
+
+    return view;
+  }
+}
