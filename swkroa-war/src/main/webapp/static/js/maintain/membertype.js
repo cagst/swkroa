75f73ebfd58fad5acd3c708ad30d8fb6f01c07e5
Craig Gaskill <craig.gaskill@gmail.com>
2015-05-19 21:44:11 -0500

    Added ability to list out Member Types in Maintain / Member Types page.

---
+++ swkroa-war/src/main/webapp/static/js/maintain/membertype.js
@@ -0,0 +1,15 @@
+/**
+ * (c) 2014 CAGST Solutions: http://www.cagst.com/solutions
+ *
+ * Author: Craig Gaskill
+ */
+
+swkroaApp.controller('membertypeController', ['$scope', '$http', function($scope, $http) {
+  $http.get('/api/membertype').success(function(data) {
+    $scope.types = data;
+  });
+
+  $scope.getType = function(type) {
+    $scope.selected = type;
+  };
+}]);
